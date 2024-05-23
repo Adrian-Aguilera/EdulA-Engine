901e2b4809e14b3da3016f42e1837c6f611687eb
Adrian-Aguilera <110703102+Adrian-Aguilera@users.noreply.github.com>
2024-05-22 22:43:35 -0600

    feat: testing ollama module with another option v1

---
+++ TModel/Ollama_Model.py
@@ -0,0 +1,16 @@
+import ollama
+
+while True:
+    messages = []
+    user_input = input("mensaje: ")
+    messages.append({"role": "system", "content": "eres un tutor digital diseñado por la institucion ITCA fepade de el salvador, ayudas a los estudiantes en cualquier duda, y solo hablas español!"})
+
+    messages.append({"role": "user", "content": user_input})
+    
+    response = ollama.chat(model='llama2:chat', messages=messages)
+    
+    chat_response = response['message']['content']
+    messages.append({"role": "assistant", "content": chat_response})
+    print(chat_response)
+    if user_input == "salir":
+        break
