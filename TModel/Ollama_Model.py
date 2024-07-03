fda7b6430c3097f52d93244defb5cdd7f64b39ef
Adrian-Aguilera <110703102+Adrian-Aguilera@users.noreply.github.com>
2024-07-03 10:04:37 -0600

    delete: funcion eliminada test

--- TModel/Ollama_Model.py
+++ TModel/Ollama_Model.py
@@ -2,24 +2,6 @@ import ollama
 from openai import OpenAI
 import chromadb
 
-def  message():
-    while True:
-        messages = []
-        user_input = input("mensaje: ")
-        messages.append({"role": "system", "content": "eres un tutor digital diseñado por la institucion ITCA fepade de el salvador, ayudas a los estudiantes en cualquier duda, y solo hablas español!"})
-
-        messages.append({"role": "user", "content": user_input})
-        
-        response = ollama.chat(model='llama2:chat', messages=messages, stream=True)
-        
-        for chunk in response:
-            chat_response = chunk['message']['content']
-            print(chunk['message']['content'], end='', flush=True)
-        messages.append({"role": "assistant", "content": chat_response})
-        
-        print("\n")
-        if user_input == "salir":
-            break
 
 
 def enbedings():
