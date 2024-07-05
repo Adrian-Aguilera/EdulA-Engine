b721060ee8d68e8fac86e86865e3759755f5c810
Adrian-Aguilera <110703102+Adrian-Aguilera@users.noreply.github.com>
2024-07-04 21:39:08 -0600

    refactor general Lmodel

--- Modules/LModel.py
+++ Modules/LModel.py
@@ -6,44 +6,6 @@ import asyncio
 class LModel():
     def __init__(self, api_key, model_point):
         self.client = OpenAI(base_url=model_point, api_key=api_key)
-        self.animation_running = False
-
-        elementos = ['-', '\\', '|', '/']
-        idx = 0
-        while self.animation_running:
-            print(elementos[idx % len(elementos)], end="\r")
-            idx += 1
-            time.sleep(0.1)
-        print(" " * 20, end="\r")
-
-    
-        messages = []
-        print("Escribe 'salir' para terminar la conversación.")
-        while True:
-            if user_question.lower() == 'salir':
-                break
-            messages.append({"role": "system", "content": sys_content})
-            messages.append({"role": "user", "content": user_question})
-
-            self.animation_running = True
-            animation_thread = threading.Thread(target=self.loading_animation)
-            animation_thread.start()
-
-            try:
-                completion = self.client.chat.completions.create(
-                    model=model,
-                    messages=messages,
-                    stream=True, 
-                    temperature=0.0,
-                    max_tokens=150,
-                )
-            finally:    
-                self.animation_running = False
-                animation_thread.join()
-
-            chat_response = completion.choices[0].message.content
-            print("Assistant:", chat_response)
-            messages.append({"role": "assistant", "content": chat_response})
             
     async def response_chat(self, model, sys_content, message_user):
         histoy_conversation = []
