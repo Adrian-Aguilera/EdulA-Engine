064d1f550afc6b360ebae5e580b871716f885a88
Adrian-Aguilera <cruz74270@gmail.com>
2024-07-05 22:16:26 -0600

    fix: cambios en modelCustom

--- TModel/ollamaCreate.py
+++ TModel/ollamaCreate.py
@@ -1,24 +1,28 @@
 from ollama import Client
 
 ollama = Client(host='127.0.0.1:11434')
+
 def modelCUSTOM():
-    modelfile='''
+    modelfile = '''
     FROM llama2:7b
-    SYSTEM eres un asistente virtual hecho por la ITCA FEPADE, y solo hablas en español
+    PARAMETER temperature 0
+    SYSTEM Eres un asistente virtual creado por ITCA FEPADE en El Salvador y solo hablas en español. Tu función es proporcionar asistencia estrictamente en temas educativos. Debes mantener un enfoque educativo en todas tus respuestas y evitar la alucinación o respuestas fuera de tema.
     '''
 
-    test = ollama.create(model='prueba', modelfile=modelfile)
-    print(test)  
+    test = ollama.create(model='generalITCA', modelfile=modelfile)
+    print(test) 
+ 
 
 def callModelcustom():
     stream = ollama.chat(
-        model='prueba',
+        model='generalITCA',
         messages=[{'role': 'user', 'content': 'quien eres? y de donde eres'}],
         stream=True,
+        options={'num_ctx':50}
     )
 
     for chunk in stream:
         print(chunk['message']['content'], end='', flush=True)
 if __name__ == "__main__":
-    callModelcustom()
+    modelCUSTOM()
     
