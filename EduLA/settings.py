3ebd5de7c96c641172b35056751747979ac2238c
edulaitca <edulaitca@gmail.com>
2024-07-09 09:25:19 -0600

    fix: ssl config

--- EduLA/settings.py
+++ EduLA/settings.py
@@ -167,10 +167,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'EduApp/static'),
 ]
 
+"""
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'http')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = True"""
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
