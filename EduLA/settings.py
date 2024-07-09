df685b6fdce37eedd687d055644095f57ad45232
Adrian-Aguilera <cruz74270@gmail.com>
2024-07-08 19:15:33 -0600

    fix: cambio de nombre app

--- EduLA/settings.py
+++ EduLA/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'EduApp',
-    'ModelCustomApp',
+    'ModelCustom',
     'rest_framework',
     'sslserver',
     'drf_yasg',
