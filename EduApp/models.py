9a954907fd068e0c8993ad78a52255dec72b5625
Adrian-Aguilera <cruz74270@gmail.com>
2024-07-16 19:42:51 -0600

    feat: config ChromaGeneral

--- EduApp/models.py
+++ EduApp/models.py
@@ -1,9 +1,14 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
 # Create your models here.
-class Conversation(models.Model):
-    user_id = models.CharField(max_length=100)
-    session_id = models.CharField(max_length=100)
-    message = models.TextField()
-    response = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+class configChromaGeneral(models.Model):
+    nameCollection = models.CharField(max_length=255)
+
+    #funcion para validar que solo se pueda ingresar una vez
+    def save(self, *args, **kwargs):
+        if self.pk is None and configChromaGeneral.objects.exists():
+            raise ValidationError('No puedes crear un nuevo registro de ConfigGeneral. Solo puedes modificar el existente.')
+        super(configChromaGeneral, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.nameCollection
