2b5b94d89149968ceae43c760705ce26231ab94d
Adrian-Aguilera <cruz74270@gmail.com>
2024-07-16 19:12:01 -0600

    FIX: CUSTOM STYLE ADMIN PANEL

--- EduLA/settings.py
+++ EduLA/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'colorfield',
+    'admin_interface',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
