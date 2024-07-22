b8e4a31a0c78aa3fa0020acf2c25a18ef4e2786f
Adrian-Aguilera <cruz74270@gmail.com>
2024-07-21 18:18:43 -0600

    feat(Nuevo Campo): campo DataChromaGeneral

--- EduApp/admin.py
+++ EduApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import configChromaGeneral, configChromaAV
+from .models import configChromaGeneral, configChromaAV, DataChromaGeneral
 # Register your models here.
 
 admin.site.register(configChromaGeneral)
 admin.site.register(configChromaAV)
+admin.site.register(DataChromaGeneral)
--- EduApp/models.py
+++ EduApp/models.py
@@ -24,3 +24,14 @@ class configChromaAV(models.Model):
 
     def __str__(self):
         return self.nameCollection
+
+class DataChromaGeneral(models.Model):
+    dataContent = models.TextField(help_text='datos que se usaran como contexto para General chat')
+
+    def save(self, *args, **kwargs):
+        if self.pk is None and configChromaGeneral.objects.exists():
+            raise ValidationError('No puedes crear un nuevo campo campo para el contenido de chatGeneral')
+        super(configChromaGeneral, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.dataContent
