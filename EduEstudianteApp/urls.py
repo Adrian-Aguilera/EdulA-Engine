480b0a070dccad7c8d75a3f4535d541055e7f5f5
Adrian-Aguilera <cruz74270@gmail.com>
2025-07-12 17:02:18 -0600

    delete tokens

--- EduEstudianteApp/urls.py
+++ EduEstudianteApp/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import LoginEstudiante, PerfilTokenObtainPairView
+from .views import LoginEstudiante
 
 urlpatterns = [
     path('login', LoginEstudiante.login, name='Inicio de sesion'),
     path('Registrar', LoginEstudiante.RegistrarEstudiante, name='Registrar un estudiante'),
-    path('Perfil/tokens', PerfilTokenObtainPairView.as_view(), name='endpoint de generacion de tokens para Estudiantes' )
 ]
