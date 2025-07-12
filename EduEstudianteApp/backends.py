219a4ec6dfc075cf06247fd9f15a8bd215cc4204
Adrian-Aguilera <cruz74270@gmail.com>
2025-07-12 17:02:02 -0600

    new backend logic

--- EduEstudianteApp/backends.py
+++ EduEstudianteApp/backends.py
@@ -2,15 +2,16 @@ from django.contrib.auth.backends import BaseBackend
 from .models import PerfilEstudiante
 
 class PerfilBackend(BaseBackend):
-    @staticmethod
     def authenticate(self, request, carnet=None, password=None, **kwargs):
         try:
-            perfil = PerfilEstudiante.objects.get(carnet=carnet)
-            if perfil.check_password(password):
-                return perfil
+            estudiante = PerfilEstudiante.objects.get(carnet=carnet)
         except PerfilEstudiante.DoesNotExist:
             return None
-    @staticmethod
+
+        if estudiante.check_password(password):
+            return estudiante
+        return None
+
     def get_user(self, user_id):
         try:
             return PerfilEstudiante.objects.get(pk=user_id)
