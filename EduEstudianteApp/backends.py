69e4ad730a78547138f3cf38c9f6e851945e54cf
Adrian-Aguilera <cruz74270@gmail.com>
2025-07-12 16:51:36 -0600

    static methods

--- EduEstudianteApp/backends.py
+++ EduEstudianteApp/backends.py
@@ -2,6 +2,7 @@ from django.contrib.auth.backends import BaseBackend
 from .models import PerfilEstudiante
 
 class PerfilBackend(BaseBackend):
+    @staticmethod
     def authenticate(self, request, carnet=None, password=None, **kwargs):
         try:
             perfil = PerfilEstudiante.objects.get(carnet=carnet)
@@ -9,7 +10,7 @@ class PerfilBackend(BaseBackend):
                 return perfil
         except PerfilEstudiante.DoesNotExist:
             return None
-
+    @staticmethod
     def get_user(self, user_id):
         try:
             return PerfilEstudiante.objects.get(pk=user_id)
